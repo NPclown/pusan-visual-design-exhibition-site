3029c489c34eb62781332b6b749676eee0cce50c
Npclown <nigrumspiritus@gmail.com>
2020-09-19 01:04:35 +0900

    Footer 수정 및 css 수정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from './Navbar'
-import '../../assets/header.css'
 
 const Header = () =>{
     return (
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,20 @@
 import React from 'react';
-import {Container, Row, Col, Image} from 'react-bootstrap'
+import {Image} from 'react-bootstrap'
 
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
+import '../assets/main.css'
 
 
 const Home = () =>{
     return (
         <div className="App">
           <Header></Header>
-          <Container className="content">
-            <Row>
-              <Col> 
+          <div className="content">
+              <div> 
                 <Image src="/image/main/main.jpg" className="Main-logo" alt="logo" fluid />
-              </Col>
-            </Row>
-          </Container>
+              </div>
+          </div>
           <Footer></Footer>
       </div>
     );
