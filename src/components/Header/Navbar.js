faa1ae5d0a16b0d04aed993b0285b51791f242a8
donghyun son <nigrumspiritus@gmail.com>
2020-09-22 21:03:22 +0900

    안쓰는 css 제거

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,18 +1,11 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { MenuItems } from "./MenuItems"
 import './Navbar.css'
 
 const Navbar = () => {
-    const [navbar, setNavbar] = useState(false)
-
-    const showNavbar = () => setNavbar(!navbar)
-
     return(
         <nav className="NavbarItems">
-            <div className="menu-icon" onClick={showNavbar}>
-                <i className={navbar ? 'fas fa-times' : 'fas fa-bars'}></i>
-            </div>
-            <ul className={navbar ? 'nav-menu active' : 'nav-menu'}>
+            <ul className='nav-menu'>
                 {MenuItems.map((item, index) => {
                     return (
                         <li key={index}>
