cc775a1faabb41c505a0c6688460fd44a4a66ead
donghyun son <nigrumspiritus@gmail.com>
2020-09-22 20:27:09 +0900

    헤더 디자인 수정
    - 검색바 추가 및 css 조정

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Navbar from './Navbar'
+import Navbar from './Navbar';
+import Searchbar from './Searchbar';
 
 const Header = () =>{
     return (
         <div className="header"> 
             <Navbar></Navbar>
+            <Searchbar></Searchbar>
         </div>
     );
 }
---
+++ src/components/Header/Searchbar.js
@@ -0,0 +1,28 @@
+
+import React from 'react';
+import {Form} from 'react-bootstrap'
+import './Searchbar.css'
+
+const Searchbar = () => {
+
+    const search = () => {
+        alert('search')   
+    }
+
+    return(
+        <div className="SearchBarItems">
+            <div className='selector'>
+                <select className='selector' name="select">
+                    <option value="이름">이름</option>
+                    <option value="작품명">작품명</option>
+                </select>
+            </div>
+            <div>
+                <input></input>
+            </div>
+            <i onClick={search} className='fas fa-search'></i>
+        </div>
+    )
+}
+
+export default Searchbar
