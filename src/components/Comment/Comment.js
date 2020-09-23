6002590a5a5d957afe216d31184aa5aff7d107bd
donghyun son <nigrumspiritus@gmail.com>
2020-09-24 00:55:39 +0900

    작품 댓글 api 적용

--- src/components/Comment/Comment.js
+++ src/components/Comment/Comment.js
@@ -1,35 +1,45 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import CommentCont from './CommentCont';
 import Axios from 'axios';
 
 import './Comment.css'
+
 const Comment = (props) =>{
-    
+    const [state, setState] = useState({isLoading : true, comments : []})
+
+    const getData = async () => {
+        Axios.get(`/api/get_article_comment?article_id=${props.idx}`)
+        .then((res) => {
+            setState({isLoading : false, comments : res.data})
+        }).catch((err) => {
+            alert(err)
+            setState({isLoading : false, comments : []})
+        }) 
+    }
     const register = () => {
         alert('댓글 등록!!');
-        Axios.post('/api/get_article_comment', {
-            article_id : 1
-        }).then((res) => {
-            alert('성공')
-            alert(res)
-        }).catch((err) => {
-            alert('실패')
-            alert(err)
-        })    
     }
 
-    return (
+    useEffect(() => {
+        getData();
+    },[])
+
+    return state.isLoading ? (
+        <div className="loading">
+            <span>Loading...</span>
+        </div>
+        ) : (
         <div className="comment">
             <div className="comment-title">
-                <div className="left">댓글(5)</div>
+                <div className="left">댓글 <span className="small">({state.comments.length})</span></div>
                 <div className="right" onClick={register}>댓글쓰기</div>                
             </div>
             <div className="comment-cont">
-                <CommentCont></CommentCont>
-                <CommentCont></CommentCont>
-                <CommentCont></CommentCont>
-                <CommentCont></CommentCont>
-                <CommentCont></CommentCont>
+                {
+                    state.comments.map((item) => (
+                        <CommentCont key={item.id} name={item.uploader_name} cont={item.comment} date="2020-09-24"></CommentCont>
+                    ))
+                }
             </div>
         </div>
     );
--- src/pages/ArtDetail.js
+++ src/pages/ArtDetail.js
@@ -5,17 +5,16 @@ import Footer from '../components/Footer/Footer'
 import ArtContent from '../components/Content/ArtContent'
 import Comment from '../components/Comment/Comment'
 import '../assets/main.css'
-import { Col } from 'react-bootstrap';
 
 
-const ArtDetail = () =>{
+const ArtDetail = (props) =>{
     return (
         <div className="App">
           <Header state="작품"></Header>
           <div className="content">
             <div>제목 및 이름 컴포넌트 자리</div>
             <ArtContent></ArtContent>
-            <Comment></Comment>
+            <Comment idx={props.match.params.idx}></Comment>
           </div>
           <Footer></Footer>
       </div>
