8ee225c31d931ec713e613c31095e457203fc95e
이현아 <hyn2626@naver.com>
2020-09-24 15:42:36 +0900

    클래스형을 함수형으로

--- src/pages/Graduate.js
+++ src/pages/Graduate.js
@@ -1,5 +1,5 @@
 //졸업자 게시판
-import React,{Component} from 'react';
+import React,{useState,useEffect} from 'react';
 import Picture from '../components/Profile/Picture'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
@@ -7,57 +7,44 @@ import '../assets/Picture.css';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
 
-class Graduate extends Component{  constructor(props) {
-  super(props);
-  console.log('in constructor');
-}
+const Graduate = (props) => {
+  const [state, setState] = useState({isLoading : true, data : []})
 
-state = {
-  data : [],
-};
+  useEffect(() => {
+    const getData = async() => {
+        try{
+            var result = await axios.get(`/api/get_profile_list`);
+            setState({isLoading : false, data : result.data})
+        } catch(error) {
+            alert(error)
+            setState({isLoading : false, data : []})
+        }
+    }
+    getData();
+},[props.id])
 
-getMyData = async () => {
-  let data = await axios.get('/api/get_profile_list');
-  data = data.data;
-  console.log('data is ' + JSON.stringify(data));
-  this.setState({data});
-};
-
-componentDidMount() {
-  console.log('in componentDidMount');
-  this.getMyData();
-}
-
-componentDidUpdate() {
-  console.log('in componentDidUpdate');
-}
-
-componentWillUnmount() {
-  console.log('in componentWillUnmount');
-}
-
-render() {
-  return (
-    <div className="Graduate">
-      <Header state="프로필"></Header>
-        <div className = "content">
-          <div className = "row1">
-            {
-              this.state.data.map((person) => {
+  return state.isLoading ? (
+    <div className="loading">
+        <span>Loading...</span>
+    </div>) : (
+        <div className="Graduate">
+          <Header state="프로필"></Header>
+            <div className = "content">
+              <div className = "row1">{
+              state.data.map((person) => {
                 return(
                   <Link to = {`profile/detail/${person.id}`}>
                   <Picture name={person.name} image={person.thumbnail_path}></Picture>
                   </Link>
                 )
-              })
-          }
-
-          </div>
-        </div>
+              }
+            )}
+              </div>
+            </div>
            <Footer></Footer>
-      </div>
+        </div>
   );
-}
+ }
 
-    }
+
   export default Graduate;
