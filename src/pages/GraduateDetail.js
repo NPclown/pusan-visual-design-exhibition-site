5ece745037079369ffdd838c9eb38ce62fca8175
Npclown <nigrumspiritus@gmail.com>
2020-10-23 02:09:26 +0900

    실제 모바일 css 수정

--- src/pages/GraduateDetail.js
+++ src/pages/GraduateDetail.js
@@ -51,7 +51,9 @@ const GraduateDetail = (props)=>{
                 vd_id={state.data.vd_id} acd_id={state.data.acd_id} ds_id={state.data.ds_id} dmd_id={state.data.dmd_id}>     
               </ArtPictureDetail>
             </div>
-            <BackArrow {...props} path="/profile"></BackArrow>
+            <div className="comment-mobile">
+              <BackArrow {...props} path="/profile"></BackArrow>
+            </div>
           </div>
           <Footer {...props} type={false}></Footer>
       </div>):(
